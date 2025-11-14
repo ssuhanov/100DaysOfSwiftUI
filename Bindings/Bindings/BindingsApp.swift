8f54ab22bba762240d5626a09f521a61e9f19b53
Sergii Sukhanov <ssuhanov@gmail.com>
2025-11-14 14:41:48 +0100

    Day 25. Bindings project

---
+++ Bindings/Bindings/BindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingsApp.swift
+//  Bindings
+//
+//  Created by Sergii Sukhanov on 14.11.25.
+//
+
+import SwiftUI
+
+@main
+struct BindingsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
