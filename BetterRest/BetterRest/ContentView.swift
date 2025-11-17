04a32c0a506bfd514c5cb5a2b2fa31e830c0a74b
Sergii Sukhanov <ssuhanov@gmail.com>
2025-11-17 17:07:58 +0100

    Day 28. Improvements for BetterRest project.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -31,84 +31,9 @@ struct ContentView: View {
   @State private var sleepAmount = 8.0
   @State private var coffeeAmount = 1
 
-  @State private var alertTitle = ""
-  @State private var alertMessage = ""
-  @State private var showingAlert = false
-
-  private static let defaultWakeTime: Date = {
-    var components = DateComponents()
-    components.hour = 7
-    components.minute = 0
-    return Calendar.current.date(from: components) ?? .now
-  }()
-
-  init() {
-    UIDatePicker.appearance().minuteInterval = 10
-  }
-
-  var body: some View {
-    NavigationStack {
-      Form {
-        VStack(alignment: .leading, spacing: .zero) {
-          Text("When do you want to wake up?")
-            .font(.headline)
-
-          DatePicker(
-            "Please enter a time",
-            selection: $wakeUp,
-            displayedComponents: .hourAndMinute
-          )
-          .labelsHidden()
-          .onDisappear {
-            UIDatePicker.appearance().minuteInterval = 1
-          }
-        }
-
-        VStack(alignment: .leading, spacing: .zero) {
-          Text("Desired amount of sleep")
-            .font(.headline)
-
-          Stepper(
-            "\(sleepAmount.formatted()) hours",
-            value: $sleepAmount,
-            in: 4...12,
-            step: 0.25
-          )
-        }
-
-        VStack(alignment: .leading, spacing: .zero) {
-          Text("Daily coffee intake")
-            .font(.headline)
-
-//          Stepper(
-//            coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
-//            value: $coffeeAmount,
-//            in: 0...20
-//          )
-          Stepper(
-            "^[\(coffeeAmount) cup](inflect: true)",
-            value: $coffeeAmount,
-            in: 0...20
-          )
-        }
-      }
-      .navigationTitle("BetterRest")
-      .backwardCompatibleNavigationSubtitle("Let us help you to sleep better!")
-      .toolbar {
-        Button("Calculate", action: calculateBedtime)
-      }
-      .alert(alertTitle, isPresented: $showingAlert) {
-        Button("OK") {}
-      } message: {
-        Text(alertMessage)
-      }
-    }
-  }
-
-  private func calculateBedtime() {
-    defer {
-      showingAlert = true
-    }
+  private var idealBedTime: (alertTitle: String, alertMessage: String) {
+    let alertTitle: String
+    let alertMessage: String
 
     do {
       let config = MLModelConfiguration()
@@ -135,7 +60,97 @@ struct ContentView: View {
       alertTitle = "Error"
       alertMessage = "Sorry, there was an issue with bedtime calculation."
     }
+
+    return (alertTitle, alertMessage)
   }
+
+  @State private var alertTitle = ""
+  @State private var alertMessage = ""
+//  @State private var showingAlert = false
+
+  private static let defaultWakeTime: Date = {
+    var components = DateComponents()
+    components.hour = 7
+    components.minute = 0
+    return Calendar.current.date(from: components) ?? .now
+  }()
+
+  init() {
+    UIDatePicker.appearance().minuteInterval = 10
+  }
+
+  var body: some View {
+    NavigationStack {
+      Form {
+        Section("When do you want to wake up?") {
+          DatePicker(
+            "Please enter a time",
+            selection: $wakeUp,
+            displayedComponents: .hourAndMinute
+          )
+          .labelsHidden()
+          .onDisappear {
+            UIDatePicker.appearance().minuteInterval = 1
+          }
+        }
+
+        Section("Desired amount of sleep") {
+          Stepper(
+            "\(sleepAmount.formatted()) hours",
+            value: $sleepAmount,
+            in: 4...12,
+            step: 0.25
+          )
+        }
+
+        Section("Daily coffee intake") {
+//          Stepper(
+//            coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+//            value: $coffeeAmount,
+//            in: 0...20
+//          )
+
+//          Stepper(
+//            "^[\(coffeeAmount) cup](inflect: true)",
+//            value: $coffeeAmount,
+//            in: 0...20
+//          )
+
+          Picker("Amount of cups", selection: $coffeeAmount) {
+            ForEach(0..<21) {
+              Text("^[\($0) cup](inflect: true)")
+            }
+          }
+          .labelsHidden()
+        }
+
+        Section("\(idealBedTime.alertTitle)") {
+          Text("\(idealBedTime.alertMessage)")
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity, alignment: .center)
+        }
+      }
+      .navigationTitle("BetterRest")
+      .backwardCompatibleNavigationSubtitle("Let us help you to sleep better!")
+//      .toolbar {
+//        Button("Calculate", action: calculateBedtime)
+//      }
+//      .alert(alertTitle, isPresented: $showingAlert) {
+//        Button("OK") {}
+//      } message: {
+//        Text(alertMessage)
+//      }
+    }
+  }
+
+//  private func calculateBedtime() {
+//    defer {
+//      showingAlert = true
+//    }
+//
+//    alertTitle = idealBedTime.alertTitle
+//    alertMessage = idealBedTime.alertMessage
+//  }
 }
 
 #Preview {
