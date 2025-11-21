9776c6c0e09c88df6eb311c4d2a1e59f780b71f4
Sergii Sukhanov <ssuhanov@gmail.com>
2025-11-21 09:26:01 +0100

    Day 32. Animations.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Sergii Sukhanov on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
