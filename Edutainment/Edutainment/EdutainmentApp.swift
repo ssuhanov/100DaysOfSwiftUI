2d7b5bbbc86adbc9c0fd2569d9dee1e99042c4ed
Sergii Sukhanov <ssuhanov@gmail.com>
2025-11-24 21:23:00 +0100

    Day 35. Start Edutainment project.

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Sergii Sukhanov on 24.11.25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
